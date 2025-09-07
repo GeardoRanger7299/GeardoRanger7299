62287e11d38245344d98dc7837b441f2f0313fbe
Seth <seth.glardon@outlook.com>
2025-09-07 08:05:00 +0000

    Finished M1T1, Been fighting with the commit function

---
+++ CSC134_HelloWorld_M1T1.cpp
@@ -0,0 +1,30 @@
+/*
+CSC 134
+M1T1 for CSC 134 online class
+SethG
+9/06/2025
+This program will greet te users.
+
+----------------------------------
+
+If extra pop ups occur:
+"stage changes" , no, push directly
+always "commit and sync"
+"if it says pull?" doesn't mattter
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // input: start the program
+    // processing: none
+    // output: 0 (no errors)
+    // special FX: Program says "Hello" to user
+
+cout << "Hello, user!" << endl; // always use letter l and not #1 because endl means "end line" not "end 1".
+
+
+    return 0; // no errors
+}
