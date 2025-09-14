eccadc4b3d904afd3ed89c8c64f98c96285e50a5
Seth <seth.glardon@outlook.com>
2025-09-14 02:58:10 +0000

    M1LAB Beginning, No errors so far

---
+++ CSC134_AppleOrchard_M1LAB.cpp
@@ -0,0 +1,32 @@
+/*
+CSC134
+M1LAB for CSC 134 online class
+SethG
+09/13/2025
+This program will advertise apples 
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // output: 0 (no errors)
+
+    //This program will simulate an apple orchard.
+    //The owner's name
+    string name = "Jane Smith";
+    //Total number of apples owned
+    int apples = 100;
+    //Price per apple
+    double pricePerApple = 0.25;
+
+    //This will calculate the total price of the apples
+    //TODO
+
+    //Print all the information about the orchard
+    //TODO
+    cout << "Welcome to " << name;
+    cout << "Jane Smith's apple orchard." << endl;
+}
