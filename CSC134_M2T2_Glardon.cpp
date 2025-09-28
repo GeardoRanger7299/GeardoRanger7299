4fa2328296648c9735b9e57898f1f90a1dc0af93
Seth <seth.glardon@outlook.com>
2025-09-28 04:34:49 +0000

    M2T2 half complete

---
+++ CSC134_M2T2_Glardon.cpp
@@ -0,0 +1,48 @@
+/*
+CSC134
+M2T2LAB for CSC 134 online class
+SethG
+09/28/2025
+This program will calculate a complete receipt
+*/
+
+#include <iostream>
+#include <iomanip> // This will be used for the decimal place. We will do 2 decimal places
+using namespace std;
+
+int main()
+
+{
+    // output 0 (no errors)
+
+    // Variables for the process
+    string item = "🥪 sub";
+    double item_price = 5.99;
+    double tax_percent = 0.08;  // 8% is 8/100
+    double tax_amount;          // tax in $
+    double total;               // price + tax
+
+
+    // Will greet the user and take their order
+    cout << "Welcome to our CSC 134 Restaurant!" << endl;
+    cout << "You ordered one " << item << "." << endl;
+
+    // Program will create a simple receipt with a calculated (8%) sales tax
+    // Calculate the total price including sales tax
+    tax_amount = item_price * tax_percent; // calculate the 8% of the item price
+    total = item_price + tax_amount;
+
+    // Print receipt
+    cout << setprecision(2) << fixed;
+    cout << "   1 sub: $" << item_price << endl;
+    cout << "   sales tax: $" << tax_amount << endl;
+    cout << "Your total is $" << total << endl;
+    cout << "Thank you for your purchase." << endl;
+
+
+
+
+
+    return 0;
+}
+
