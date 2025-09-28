c4d76b79aab0b805f269f12a5f91f7a8b8738652
Seth <seth.glardon@outlook.com>
2025-09-28 20:42:20 +0000

    M3T2 end, established the winning and losing criteria of "Craps dice game"

--- CSC134_M3T2_RandomNumbers_.cpp
+++ CSC134_M3T2_RandomNumbers_.cpp
@@ -3,7 +3,7 @@ CSC134
 M3T2 for CSC 134 online class
 SethG
 09/28/2025
-This program will do dice rolls
+This program will do dice rolls for "Craps dice game"
 */
 
 #include <iostream>
@@ -21,22 +21,49 @@ int main()
 {
 
     cout << "Let's roll some dice!" << endl;
-    int seed;
-    cout << "What is your lucky number?" << endl;
-    cin >> seed;
+    int seed = time(0);
+    //cout << "The seed is; " << seed << endl;
+    //cout << "What is your lucky number?" << endl;
+    //cin >> seed;
     // Seed the RNG
     srand(seed); // Srand - seeds the random number generator with the given number.
 
 
     const int MAX = 6; // Standard D6. Ranges from 1-6
-    int roll;
+    int roll_1, roll_2, total;
 
-    roll = (rand() % MAX) + 1; // Divide by Max, just keep remainder
-    cout << "Your roll was: " << roll << endl;
-    roll = (rand() % MAX) + 1;
-    cout << "and: " << roll << endl;
-    roll = (rand() % MAX) + 1;
-    cout << "and: " << roll << endl;
+    roll_1 = (rand() % MAX) + 1; // Divide by Max, just keep remainder
+    cout << "Your roll was: " << roll_1 << endl;
+    roll_2 = (rand() % MAX) + 1;
+    cout << "and: " << roll_2 << endl;
+    total = roll_1 + roll_2;
+    cout << "Your total roll is: " << total << endl;
+
+
+    //Let's play a game of Craps!
+    // Win! - 7 and 11
+    // Lose! - 2 and 12
+
+    // Will be working on the other parts later.
+    if (total ==7) {
+        cout << "Big red! You win!" << endl;
+    }
+    else if (total ==11) {
+        cout << "Winner with an eleven!" << endl;
+    }
+    else if (total ==2) {
+        cout << "Snake eyes! You lose, better luck next time!" << endl;
+    }
+    else if (total ==3) {
+        cout << "Ace-Duece, You lose!" << endl;
+    }
+    else if (total ==12) {
+        cout << "Midnight! That is a loss!" << endl;
+    }
+    else {
+        // Anything else
+        cout << "Your point is " << total << " This will be worked on later" << endl;
+    }
 
 
 
