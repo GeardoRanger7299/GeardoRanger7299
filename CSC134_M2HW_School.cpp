b68984fd875431fb69cfc1e3bf4c58862b11978c
Seth <seth.glardon@outlook.com>
2025-10-06 01:23:51 +0000

    M2HW School end, everything works

---
+++ CSC134_M2HW_School.cpp
@@ -0,0 +1,39 @@
+/*
+CSC134
+M2HW4 - Gold
+SethG
+10/05/2025
+This program will do a chant for the school sport team
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+string letsGo,
+school,
+team,
+cheerOne,
+cheerTwo;
+
+letsGo = "Let's Go";
+school = "FTCC";
+team = "Trojans";
+
+
+cheerOne = letsGo + " " + school;
+cheerTwo = letsGo + " " + team;
+
+//Using string concatenation to make sports chant
+cout << cheerOne << endl;
+cout << cheerOne << endl;
+cout << cheerOne << endl;
+cout << cheerTwo << endl;
+cout << endl;
+
+return 0;
+
+}
