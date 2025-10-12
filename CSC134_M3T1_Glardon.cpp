0a701da255a13001996d64c0b023b53ae764bf77
Seth <seth.glardon@outlook.com>
2025-10-12 23:12:03 +0000

    M3T1 end, additions made. everything tested and works fine

--- CSC134_M3T1_Glardon.cpp
+++ CSC134_M3T1_Glardon.cpp
@@ -18,12 +18,12 @@ int main()
     int area_rec_one, area_rec_two;
 
     //User input
-    cout << "Please enter the length and width of the first rectangle," << endl;
-    cout << " separated by a space or new line" << endl;
+    cout << "Please enter the length and width of rectangle one," << endl;
+    cout << "separated by a space or new line." << endl;
     cin >> length_rec_one >> width_rec_one;
 
-    cout << "Please enter the length and width of the second rectangle," << endl;
-    cout << " separated by a space or new line" << endl;
+    cout << "Please enter the length and width of rectangle two," << endl;
+    cout << " separated by a space or new line." << endl;
     cin >> length_rec_two >> width_rec_two;
 
     //Calculations based off user input
@@ -31,8 +31,26 @@ int main()
     area_rec_two = length_rec_two * width_rec_two;
 
     //Results from calculations
-    cout << "The area of the first rectangle is " << area_rec_one << endl;
-    cout << "The area of the second rectangle is " << area_rec_two << endl;
+    cout << endl;
+    cout << "The area of rectangle one is " << area_rec_one << endl;
+    cout << "The area of rectangle two is " << area_rec_two << endl;
+
+    //which rectangle is bigger
+    if (area_rec_one > area_rec_two) {
+        cout << "The area of rectangle one is bigger." << endl;
+    }
+
+    else if (area_rec_two > area_rec_one) {
+            cout << "The area of rectangle two is bigger." << endl;
+        }
+
+    else if (area_rec_one == area_rec_two) {
+        cout << "The area of both rectangles are the same." << endl;
+    }
+
+    cout << endl;
+    cout << "Thank you for your time. Have a nice day!" << endl;
+
 
     return 0;
 
