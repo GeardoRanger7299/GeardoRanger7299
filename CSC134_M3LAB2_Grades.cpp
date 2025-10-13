12fc231e5e07a55253f24e8441f63673ead30a9e
Seth <seth.glardon@outlook.com>
2025-10-13 01:34:51 +0000

    M3LAB2 middle, additions made no test yet

--- CSC134_M3LAB2_Grades.cpp
+++ CSC134_M3LAB2_Grades.cpp
@@ -20,22 +20,30 @@ int main()
     cout << "Please enter your number grade." << endl;
     cout << "The number grade will range from 0 to 100." << endl;
     cout << "Number grade: " << endl;
+    cin >> number_grade;
+    cout << endl;
 
     //Letter grade based oof number using "if" and "boolean" operations
     if (number_grade >= 90 && number_grade <= 100) {
-
+        cout << "Your letter grade is A." << endl;
     }
     else if (number_grade >= 80 && number_grade <= 89) {
-
+        cout << "Your letter grade is B." << endl;
     }
     else if (number_grade >= 70 && number_grade <= 79) {
-
+        cout << "Your letter grade is C." << endl;
     }
     else if (number_grade >= 60 && number_grade <= 69) {
-
+        cout << "Your letter grade is D." << endl;
     }
     else if (number_grade >= 0 && number_grade <= 59) {
-
+        cout << "Your letter grade is F." << endl;
     }
+    else {
+        cout << "The user input is not valid. Please restart process." << endl;
+    }
+
+
+    return 0;
 
 }
