49859d5bdd8febcb5ccad894971a6c0d3e299e43
Seth <seth.glardon@outlook.com>
2025-10-13 01:28:55 +0000

    M3LAB2 middle, setup framework

---
+++ CSC134_M3LAB2_Grades.cpp
@@ -0,0 +1,41 @@
+/*
+CSC134
+M3Lab2 for CSC 134 online class
+SethG
+10/12/2025
+This program will give a letter grade for the respective letter grade
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+    //Variables
+    int number_grade;
+
+    //Asking for user input
+    cout << "Please enter your number grade." << endl;
+    cout << "The number grade will range from 0 to 100." << endl;
+    cout << "Number grade: " << endl;
+
+    //Letter grade based oof number using "if" and "boolean" operations
+    if (number_grade >= 90 && number_grade <= 100) {
+
+    }
+    else if (number_grade >= 80 && number_grade <= 89) {
+
+    }
+    else if (number_grade >= 70 && number_grade <= 79) {
+
+    }
+    else if (number_grade >= 60 && number_grade <= 69) {
+
+    }
+    else if (number_grade >= 0 && number_grade <= 59) {
+
+    }
+
+}
