edcc593b84e0ccabed0fb0489912c8e5125be944
Seth <seth.glardon@outlook.com>
2025-10-19 23:19:49 +0000

    M3HW middle,  second question done

--- CSC134_M3HW.cpp
+++ CSC134_M3HW.cpp
@@ -1,17 +1,22 @@
 /*
 CSC134
-M2HW2 - Gold
+M3HW - Gold
 SethG
 10/19/2025
-This program will act as a super simple chat bot.
+This program will answer 4 homework questions.
 */
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 
 {
+
+    //First question of the assignment.
+    cout <<"Question #1" << endl;
+    //This program will act as a super simple chat bot.
 
     //Variables
     string choice, yes, no;
@@ -19,6 +24,7 @@ int main()
     //Greet user
     cout << "Hello, I am Jarvis, a simple C++ chatbot program!" << endl;
     cout << "Would you like to be my friend? Please answer yesr or no." << endl;
+    cout << "Input: ";
     cin >> choice;
 
     //Responses
@@ -32,6 +38,58 @@ int main()
         cout << "That is not a valid answer to the question." << endl;
         cout << "If you are not sure you can always come back later." << endl;
     }
+    cout << endl;
+
+    //Second question of the assignment.
+    cout << "Question #2" << endl;
+    //This will calculate the information for a receipt.
+
+     // Variables for the process
+    int order_choice;
+    double item_price;
+    double tax_percent = 0.08;  // 8% is 8/100
+    double tax_amount;          // tax in $
+    double tip_amount = 0.00;
+    double total;               // price + tax
+
+
+    // Will greet the user and take their order
+    cout << "Welcome to our CSC 134 Restaurant!" << endl;
+
+    //Request user input
+    cout << "Please enter the price of the meal." << endl;
+    cout << "Meal price: ";
+    cin >> item_price;
+    cout << endl;
+
+    cout << "Eneter 1 for dine in or 2 for to go." << endl;
+    cin >> order_choice;
+    cout << endl;
+
+    // Calculate the total price including sales tax
+    tax_amount = item_price * tax_percent; // calculate the 8% of the item price
+    total = item_price + tax_amount;
+    double tip_percent = 0.15;
+
+    if (order_choice == 1) {
+        tip_amount = item_price * tip_percent;
+    }
+    else if (order_choice == 2) {
+        tip_amount = item_price * 0;
+    }
+
+    // Print receipt
+    cout << setprecision(2) << fixed;
+    cout << "********************" << endl;
+    cout << "   Subtotal:" << "\t\t$" << item_price << endl;
+    cout << "   Tax:" << "\t\t\t$" << tax_amount << endl;
+    cout << "   Tip:" << "\t\t\t$" << tip_amount << endl;
+    cout << "   Your total is:" << "\t$" << total << endl;
+    cout << "____________________" << endl;
+    cout << "Thank you for your purchase." << endl;
+    cout << endl;
+
+
 
     return 0;
 
