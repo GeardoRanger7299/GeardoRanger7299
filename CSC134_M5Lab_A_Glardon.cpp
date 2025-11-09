2d460881664acaea8d8f698449772320a95660e6
Seth <seth.glardon@outlook.com>
2025-11-09 19:42:01 +0000

    M5LabA middle, exercise 1 complete

--- CSC134_M5Lab_A_Glardon.cpp
+++ CSC134_M5Lab_A_Glardon.cpp
@@ -7,10 +7,22 @@ int main()
 
     int LP = 30;
     int max_LP = 100;
+    const int recover = 10;
+
+    string current_LP = "*";
+    string missing_LP = "°";
 
     while (LP < max_LP) {
-        LP = LP + 10;
-        cout << "LP: " << LP << "/" << max_LP << endl;
+        LP += recover;  
+        // Draw 10 blocks, full and empty depending on HP
+        
+        for (int i=0 ; i < LP/10; i++) {
+            cout << current_LP;
+        }
+        for (int i=0; i < (100-LP)/10; i++) {
+            cout << missing_LP;
+        }
+        
+        cout << " LP: " << LP << "/" << max_LP << endl;
     }
-    cout << "LP full!" << endl;
 }
