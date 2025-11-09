caadfc6d1ecfab182bb70aab43b0dcd415d5e2b6
Seth <seth.glardon@outlook.com>
2025-11-09 13:47:14 +0000

    M5T1 start, wrote out example 1

---
+++ M5T1_Glardon.cpp
@@ -0,0 +1,23 @@
+/*
+CSC134
+M3T1 for CSC 134 online class
+SethG
+11/09/2025
+This program will be a practice assignment for functions
+*/
+
+#include <iostream>
+using namespace std;
+
+int doubleANumber (int num);
+
+int main() {
+    int number = 4
+    number = doubleANumber (number);
+    cout << number << endl;
+    return 0;
+}
+int doubleANumber (int num) {
+    num = num * 2;
+    return num;
+}
