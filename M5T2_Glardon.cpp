1b8388adfa2e2e5a72ffc4d051e457cb8ceff694
Seth <seth.glardon@outlook.com>
2025-11-09 16:18:45 +0000

    M5T2 end

--- M5T2_Glardon.cpp
+++ M5T2_Glardon.cpp
@@ -1,14 +1,16 @@
 /*
 CSC134
-M3T1 for CSC 134 online class
+M5T2 for CSC 134 online class
 SethG
 11/09/2025
 This program will be the second practice assignment for functions
 */
 
 #include <iostream>
+#include <math.h>
 using namespace std;
 
+/*
 void printResult (int number, int result) {
     cout << number << " squared = " << result << endl;
 }
@@ -27,3 +29,57 @@ int main()
         count++;
     }
 }
+*/
+
+
+void printResult (int number, int result) {
+    cout << number << " squared = " << result << endl;
+}
+
+//Give a number and return the square of that number
+int square (int number) {
+    int result;
+    result = number * number;
+    return result;
+}
+
+int main()
+
+{
+
+    int count = 1;
+    int result;
+
+    while (count <=10) {
+        result = square (count);
+        printResult (count, result);
+        count++;
+    }
+}
+
+
+/*
+//An alternative way of doing the same thing the previous version does but as exponential growth
+void printResult (int number, int result) {
+    cout << number << " : " << result << endl;
+}
+
+int growth (int number) {
+    int result = pow ((double) 2 , (double) number);
+    return result;
+}
+
+int main()
+// Exponential growth instead of squared
+{
+
+    int number, result;
+    number = 1;
+
+    while (number <=10) {
+        result = growth (number);
+        printResult (number, result);
+        number++;
+    }
+}
+*/
