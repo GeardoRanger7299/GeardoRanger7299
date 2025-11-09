0b61d894d616a4fa068cbbe70e99b70cf0623934
Seth <seth.glardon@outlook.com>
2025-11-09 20:13:12 +0000

    M5LabA middle, have the basis but need to tweak code

--- CSC134_M5Lab_A_Glardon.cpp
+++ CSC134_M5Lab_A_Glardon.cpp
@@ -8,13 +8,13 @@ int main()
     int LP = 30;
     int max_LP = 100;
     const int recover = 10;
+    
 
     string current_LP = "*";
     string missing_LP = "°";
 
     while (LP < max_LP) {
         LP += recover;  
-        // Draw 10 blocks, full and empty depending on HP
         
         for (int i=0 ; i < LP/10; i++) {
             cout << current_LP;
@@ -24,5 +24,34 @@ int main()
         }
         
         cout << " LP: " << LP << "/" << max_LP << endl;
+
+        string choice;
+        cout << "Do you want to rest and recover LP?" << endl;
+        cout << "yes or no: ";
+        cin >> choice;
+
+        if (choice == "yes" || choice == "yes") {
+            LP += recover;
+            if (LP > max_LP) {
+                LP = max_LP;
+            }
+            cout << "You rested. " << recover << " LP recovered." << endl << endl;
+        } 
+        
+        else {
+            cout << "You chose not to rest. Recovery halted." << endl;
+            break; 
+        }
     }
+    
+    if (LP == max_LP) {
+        for (int i = 0; i < LP / 10; i++) {
+            cout << current_LP;
+        }
+        cout << " LP: " << LP << "/" << max_LP << endl;
+        cout << "LP fully recovered!" << endl;
+    }
+
+    return 0;
+ 
 }
