53ba569c6aa3557b0441bad902df87fbb7c68330
Seth <seth.glardon@outlook.com>
2025-11-09 15:23:45 +0000

    M5T2 start, part 1

---
+++ M5T2_Glardon.cpp
@@ -0,0 +1,30 @@
+/*
+CSC134
+M3T1 for CSC 134 online class
+SethG
+11/09/2025
+This program will be the second practice assignment for functions
+*/
+
+#include <iostream>
+using namespace std;
+
+void OutputMinutesAsHours (double origMinutes) {
+
+
+
+}
+
+int main()
+
+{
+
+    double minutes;
+
+    cin >> minutes;
+
+    OutputMinutesAsHours (minutes);
+    cout << endl;
+
+    return 0;
+}
