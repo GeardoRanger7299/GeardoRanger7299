beec3ddefbf3755dad20f0819b2e9bda50f9f7c1
Seth <seth.glardon@outlook.com>
2025-11-15 21:18:35 +0000

    M5HW start, added basic structure for question 1

---
+++ CSC134_M5HW.cpp
@@ -0,0 +1,30 @@
+/*
+CSC134
+M5HW Gold
+SethG
+11/15/2025
+This program 
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+
+{
+
+    //Rain variables
+    string Month_1, Month_2, Month_3;
+    double RainfallMonth_1, RainfallMonth_2, RainfallMonth_3;
+    double AverageRainfall;
+
+    cout << "Please enter the first month." << endl;
+    cout << "Month 1: ";
+    cin >> Month_1;
+    cout << "Please enter the rainfall for that month." << endl;
+    cout << "Rainfall: ";
+    cin >> RainfallMonth_1;
+}
