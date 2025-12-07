5e6a28e49285362e0b3177ff71499abf993e539e
Seth <seth.glardon@outlook.com>
2025-12-07 18:58:46 +0000

    M6HW1 middle, added branching options

--- CSC134_M6HW1.cpp
+++ CSC134_M6HW1.cpp
@@ -22,7 +22,7 @@ and items used throughout the games. For the sake of making this simple
 and a little easier to do I will only be "Humanity" and "Boss souls"
 similar to that of the 1st game
 */
-int humanity = 0;
+int Humanity = 0;
 const int Max_Humanity = 10;
 /*
 Even though the max humanity in DS1 is 99, for
@@ -59,5 +59,43 @@ cout << "This statue appeared to depict two bodies fused together into an abomin
 cout << "One pointed North while the other Pointed South." << endl;
 cout << "There is a white soapstone sign at the bottom." << endl;
 cout << "It say North to Undead Burg and South to City Ruins." << endl;
+
+return 0;
+
+void Firelink_Shrine () {
+
+    int choice;
+
+    cout << "-{Firelink Shrine}-" << endl;
+    cout << "The faint fading bonfire warms you as you are sitting there." << endl;
+    cout << "Humanity level: " << Humanity << "/" << Max_Humanity << endl;
+    cout << "1. Head North to the Undead Burg." << endl;
+    cout << "2. Head South to the city ruins." << endl;
+    cout << "3. " << endl;
+    cout << "What will you do?" << endl;
+    cout << "Choice: ";
+    cin >> choice;
+
+    if (choice == 1) {
+        Undead_Burg ();
+    }
+    else if (choice == 2) {
+        New_Londo_Ruins ();
+    }
+    else if (choice == 3) {
+        
+    }
+    else {
+        cout << "Your will wavers. Forge your path";
+        Firelink_Shrine ();
+    }
+}
+
+void Undead_Burg () {
+
+    int choice;
+
+    cout << "-{Undead Burg}-" << endl;
+}
 
 }
